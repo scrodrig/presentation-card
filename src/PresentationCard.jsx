9c3588b86a5d90abeddd20754b7908b058663ed7
Schubert D. Rodriguez <schubert.rodriguez@outlook.com>
2022-10-19 22:37:06 +0100

    add styles and set an presentation card

--- src/PresentationCard.jsx
+++ src/PresentationCard.jsx
@@ -1,7 +1,12 @@
+import avatarImage from './assets/avatar.png'
+import './presentationCard.css'
+
 function PresentationCard(){
+    let name = 'Schubert'
     return(
-        <div>
-            Hello World
+        <div className="presentation-card">
+            <img src={avatarImage} alt='avatar' className='card-avatar'></img>
+           <h3 className='card-about'>Hi, my name is {name} and I'm remembering React</h3>
         </div>
     )
 }
